4e546a32a22bc7d6eb552c6b63854ce5153f3fb2
vlixdivers-dotcom <vlixdivers@gmail.com>
2025-11-05 22:01:29 +0100

    lock

--- src/main.js
+++ src/main.js
@@ -30,8 +30,9 @@ const config = {
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width:320,
-        height:480
+        scene: (scene) => {
+            scene.lockOrientation('portrait');
+        }
     },
 }
 
