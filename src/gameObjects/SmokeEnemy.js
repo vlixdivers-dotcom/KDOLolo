a2f363ae009520f5866fe222cb734a6b7f27f260
vlixdivers-dotcom <vlixdivers@gmail.com>
2025-11-23 20:24:40 +0100

    smaller smoke

--- src/gameObjects/SmokeEnemy.js
+++ src/gameObjects/SmokeEnemy.js
@@ -11,7 +11,8 @@ export default class SmokeEnemy extends EnemyFlying {
         this.power = 0;
         this.setTexture(ASSETS.spritesheet.smokeScreen.key, 1);
         this.anims.play(ANIMATION.smokeScreen.key);
-        this.setSize(40,40);
+        this.setOrigin(0.5,0)
+        this.setSize(this.width,this.height/2);
 
         this.setDataEnabled();
         this.setData('enemyType', 'smoke');
